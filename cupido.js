b4086f89fd651df47b8fa8a358b18d2c6029a1ad
Hector Zelaya <inge.zelaya@gmail.com>
2023-04-21 14:17:41 -0600

    added video conferencing capability

---
+++ cupido.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', socket => {
+  console.log('a user connected to the server', socket.id);
+
+  socket.broadcast.emit('event', {type: 'join', from: socket.id});
+
+  socket.on('event', evt => {
+    console.log('a signaling event was sent:', JSON.stringify(evt.type));
+    io.sockets.sockets.get(evt.to).emit('event', evt);
+  });
+});
+
+http.listen(port || 3000, () => {
+  console.log('listening on', port);
+});
